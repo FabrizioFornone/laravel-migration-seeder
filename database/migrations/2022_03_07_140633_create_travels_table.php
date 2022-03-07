83ae85835ab39feb4fc7a76ed98f5aaf2f91354c
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-03-07 15:29:42 +0100

    php artisan migrate

---
+++ database/migrations/2022_03_07_140633_create_travels_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('dealers', 100);
+            $table->string('location', 100);
+            $table->date('departure');
+            $table->date('arrival');
+            $table->float('price', 8, 2);
+            $table->text('travel description');
+            $table->boolean('available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
